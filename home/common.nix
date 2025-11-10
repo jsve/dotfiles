ef3dc40adcbe6a796afffb625d41cb30f2087abc
Johan Svensson <j_svensson@me.com>
2025-11-10 12:33:44 +0100

    Update common.nix

--- home/common.nix
+++ home/common.nix
@@ -1,30 +1,33 @@
 { pkgs, lib, ... }:
 {
-    home.stateVersion = "25.05";
+  home.stateVersion = "25.05";
 
-    # Let Home Manager install and manage itself.
-    programs.home-manager.enable = true;
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 
-    imports = [
-      ./modules/vscode.nix
-      ./modules/zsh.nix
-    ];
+  imports = [
+    ./modules/vscode.nix
+    ./modules/zsh.nix
+  ];
 
-    home.packages = with pkgs; [
-      # Haxx
-      bruno
-      gh
-      neovim
-      openssl
-      opencode
-      orbstack
-      # ghostty <- managed by homebrew
+  home.packages = with pkgs; [
+    # Haxx
+    bruno
+    gh
+    neovim
+    openssl
+    opencode
+    orbstack
 
-      # Entertainment
-      # steam <- managed by homebrew
+    # ch:
+    nodejs_20
+    python310
+    yarn
+    pnpm_8
 
-      # Communication
-      # slack
-    ];
+    # managed by homebrew
+    # ghostty
+    # steam
+  ];
 
 }
